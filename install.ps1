2db529e96c0ec124a46ce16f1e3fbe2f7def8af6
Sora-bluesky <komei1118ph@gmail.com>
2026-04-05 00:59:15 +0900

    chore: bump version to 0.18.0

--- install.ps1
+++ install.ps1
@@ -10,7 +10,7 @@ param(
 $ErrorActionPreference = 'Stop'
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$VERSION      = "0.17.4"
+$VERSION      = "0.18.0"
 $WINSMUX_DIR  = Join-Path $HOME ".winsmux"
 $BIN_DIR      = Join-Path $WINSMUX_DIR "bin"
 $BACKUP_DIR   = Join-Path $WINSMUX_DIR "backups"
