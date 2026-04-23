f4f6e0d397f6a6651c9d3df638af879f509e7d0b
Sora-bluesky <komei1118ph@gmail.com>
2026-04-23 22:42:22 +0900

    chore: bump version to 0.24.0

--- scripts/winsmux-core.ps1
+++ scripts/winsmux-core.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 # --- Config ---
-$VERSION = "0.23.1"
+$VERSION = "0.24.0"
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $ErrorActionPreference = 'Stop'
 $BridgeScriptPath = $PSCommandPath
