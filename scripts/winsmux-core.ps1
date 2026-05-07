33e7534af30c8c3bf2b26d8138fbad61177d6a92
Sora-bluesky <komei1118ph@gmail.com>
2026-05-08 03:48:17 +0900

    chore: bump version to 0.25.5

--- scripts/winsmux-core.ps1
+++ scripts/winsmux-core.ps1
@@ -17,7 +17,7 @@ if (-not [string]::IsNullOrWhiteSpace($env:WINSMUX_RAW_EXE)) {
 }
 
 # --- Config ---
-$VERSION = "0.25.4"
+$VERSION = "0.25.5"
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $ErrorActionPreference = 'Stop'
 $BridgeScriptPath = $PSCommandPath
