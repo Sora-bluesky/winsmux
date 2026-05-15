db113a0e8db4e7ef4a48b2e236fdd3889eedabf2
Sora-bluesky <komei1118ph@gmail.com>
2026-05-15 12:38:28 +0900

    chore: bump version to 0.33.2

--- install.ps1
+++ install.ps1
@@ -12,7 +12,7 @@ param(
 $ErrorActionPreference = 'Stop'
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-$VERSION      = "0.33.1"
+$VERSION      = "0.33.2"
 $WINSMUX_DIR  = Join-Path $HOME ".winsmux"
 $BIN_DIR      = Join-Path $WINSMUX_DIR "bin"
 $BACKUP_DIR   = Join-Path $WINSMUX_DIR "backups"
