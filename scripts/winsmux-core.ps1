33b92e701946ab8db730d4f9397a136d9e7e4a49
Sora-bluesky <komei1118ph@gmail.com>
2026-08-28 11:03:30 +0900

    chore(release): bump version to 0.36.37

--- scripts/winsmux-core.ps1
+++ scripts/winsmux-core.ps1
@@ -54,7 +54,7 @@ function Resolve-WinsmuxRawCommand {
 $script:WinsmuxRawCommand = $null
 
 # --- Config ---
-$VERSION = "0.36.36"
+$VERSION = "0.36.37"
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $ErrorActionPreference = 'Stop'
 $BridgeScriptPath = $PSCommandPath
